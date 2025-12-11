1c166cf74a3e3e2e273e0fed4f95bd9ebd491ceb
TheTamo <thetamoedit@gmail.com>
2025-12-11 09:00:24 +0330

    resize the monitor

--- Scripts/Player/MainPlayer.gd
+++ Scripts/Player/MainPlayer.gd
@@ -5,7 +5,6 @@ var input
 @export var speed = 100.0
 @export var gravity = 10
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
